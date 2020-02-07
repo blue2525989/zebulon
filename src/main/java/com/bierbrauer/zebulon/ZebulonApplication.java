97ea39be70491bdd08f2498e51becb7c1d7af032
jason <jbierbrauer99@gmail.com>
2020-02-06 19:47:24 -0600

    initializing the repository

---
+++ src/main/java/com/bierbrauer/zebulon/ZebulonApplication.java
@@ -0,0 +1,13 @@
+package com.bierbrauer.zebulon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZebulonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZebulonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bierbrauer/zebulon/ZebulonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bierbrauer.zebulon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZebulonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
